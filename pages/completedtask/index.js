0b60379b7b9d211b57a364e4f2f8be259653e4d4
abdullah-mamun369 <maamamun369@gmail.com>
2022-12-30 18:07:51 +0600

    first commit

---
+++ pages/completedtask/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CompleteTask = () => {
+    return (
+        <div>
+            This is CompleteTask
+        </div>
+    );
+};
+
+export default CompleteTask;
